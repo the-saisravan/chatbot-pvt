163ecb0a32e238f1c2c8c7701af6bb5235028359
Gnaneswar Sai <gnaneswarsai117@gmail.com>
2025-03-27 15:13:08 +0530

    Code Format

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { GoogleGenAI } = require("@google/genai") ;
+const { GoogleGenAI } = require("@google/genai");
 require("dotenv").config();
 
 const express = require("express");
@@ -8,30 +8,29 @@ const path = require("path");
 const app = express();
 const cors = require("cors");
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-
 app.post("/chat", async (req, res) => {
   const ai = new GoogleGenAI({ apiKey: process.env.MY_API_KEY });
-  let {userPrompt} = req.body
-    const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: userPrompt,
-      config:{
-        systemInstruction: "Provide Content in HTML format without head, html and body tags. Just Format text using relevant HTML elements give some line breaks for clear information",
-      },
-    });
-    console.log(userPrompt);
-    let result = response.text
-    console.log(result);
-    return res.send({"message": result}).json();
+  let { userPrompt } = req.body;
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: userPrompt,
+    config: {
+      systemInstruction:
+        "Provide Content in HTML format without head, html and body tags. Just Format text using relevant HTML elements give some line breaks for clear information",
+    },
+  });
+  console.log(userPrompt);
+  let result = response.text;
+  console.log(result);
+  return res.send({ message: result }).json();
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require("./app")
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+const app = require("./app");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
